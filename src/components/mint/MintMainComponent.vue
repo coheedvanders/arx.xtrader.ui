63be19016c91266545d05cb871bbe2c7bc72cc62
Miguel Ramos <coheedramos@gmail.com>
2025-11-28 21:05:22 +0800

    increment version

--- src/components/mint/MintMainComponent.vue
+++ src/components/mint/MintMainComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-center text-secondary">
-        <label>v1.53</label>
+        <label>v1.54</label>
     </div>
     <SymbolSocketComponent 
         :symbol="MASTER_SYMBOL" 
