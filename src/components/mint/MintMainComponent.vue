9865e9a3d8d270ed5b0c21796e86b78c561569fa
Miguel Ramos <coheedramos@gmail.com>
2025-12-03 23:53:54 +0800

    adjusted short1,2 tp with atr

--- src/components/mint/MintMainComponent.vue
+++ src/components/mint/MintMainComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-center text-secondary">
-        <label>v1.59</label>
+        <label>v1.60</label>
     </div>
     <SymbolSocketComponent 
         :symbol="MASTER_SYMBOL" 
