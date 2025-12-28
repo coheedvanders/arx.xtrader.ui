113e1c1606964a24f17e9b4739ff0921c504d9de
Miguel Ramos <coheedramos@gmail.com>
2025-12-28 12:47:05 +0800

    v1.78E: reduced max init to 210

--- src/components/mint/MintMainComponent.vue
+++ src/components/mint/MintMainComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-center text-secondary">
-        <label>v1.78D</label>
+        <label>v1.78E</label>
     </div>
     <SymbolSocketComponent 
         :symbol="MASTER_SYMBOL" 
@@ -152,7 +152,7 @@ const isBotEnabled = ref(false)
 
 const MASTER_SYMBOL = "BTCUSDT";
 const KLINE_INTERVAL = "15m"
-const MAX_INIT_CANDLES = 1000;
+const MAX_INIT_CANDLES = 210;
 const SUPPORT_AND_RESISTANCE_PERIOD_LENGTH = 10;
 
 const MARGIN = 1;
