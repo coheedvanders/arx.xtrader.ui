3ddf05fde890cc0ec93df6c31a307b71e5bb7338
Miguel Ramos <coheedramos@gmail.com>
2026-03-05 23:27:42 +0800

    v1.9 go my chwic

--- src/components/mint/MintMainComponent.vue
+++ src/components/mint/MintMainComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-center text-secondary">
-        <label>v1.9 - JamuymuyChucman</label>
+        <label>v1.9 - go my chwic</label>
     </div>
     <SymbolSocketComponent 
         :symbol="MASTER_SYMBOL" 
